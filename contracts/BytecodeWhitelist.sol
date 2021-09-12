8da90b50f435c1a9ec913404221af992a06c3f20
hnamzian <namazian87@gmail.com>
2021-09-12 22:50:04 +0300

    Add _whitelistedBytecodes mapping and its getter as whitelist of hash of bytecodes

--- contracts/BytecodeWhitelist.sol
+++ contracts/BytecodeWhitelist.sol
@@ -5,4 +5,10 @@ pragma solidity ^0.8.6;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BytecodeWhitelist is Ownable {
+    mapping(bytes32 => bool) internal _whitelistedBytecodes;
+
+    function isWhitelisted(bytes memory bytecode_) public view returns (bool) {
+        bytes32 _bytecodeHash = keccak256(abi.encodePacked(bytecode_));
+        return _whitelistedBytecodes[_bytecodeHash];
+    }
 }
