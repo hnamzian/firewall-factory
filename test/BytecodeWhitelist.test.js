3595c6914a3bbcc611c317aab14610067ea65f56
hnamzian <namazian87@gmail.com>
2021-09-12 23:05:09 +0300

    Test whitelisting bytecode

---
+++ test/BytecodeWhitelist.test.js
@@ -0,0 +1,17 @@
+const { solidity } = require("ethereum-waffle");
+const { expect } = require("chai").use(solidity);
+const sample_bytecode = require("./bytecode");
+
+let bytecodeWhitelist;
+
+describe("BytecodeWhitelist", async () => {
+  beforeEach(async () => {
+    const BytecodeWhitelist = await ethers.getContractFactory("BytecodeWhitelist");
+    bytecodeWhitelist = await BytecodeWhitelist.deploy();
+  });
+  it("should whitelist sample_bytecode", async () => {
+    await bytecodeWhitelist.whitelistBytecode(sample_bytecode);
+    const isWhitelisted = await bytecodeWhitelist["isWhitelisted(bytes)"](sample_bytecode);
+    expect(isWhitelisted).to.be.equal(true);
+  })
+});
